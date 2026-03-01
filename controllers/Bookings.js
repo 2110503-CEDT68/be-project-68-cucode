50076a9300263be3f6293004d3146cd8699a0a09
Narinthon Yangngam <tedd0781@gmail.com>
2026-03-01 16:38:57 +0700

    fix: return 201 Created status code for addBooking
    Changed response status from 200 (OK) to 201 (Created) when a new
    booking is successfully created. This follows HTTP REST conventions
    where POST requests that create a resource should return 201.

--- controllers/Bookings.js
+++ controllers/Bookings.js
@@ -96,7 +96,7 @@ exports.addBooking = async (req, res, next) => {
         }
 
         const booking = await Booking.create(req.body);
-        res.status(200).json({ success: true, data: booking });
+        res.status(201).json({ success: true, data: booking });
 
     } catch (error) {
         console.log(error);
